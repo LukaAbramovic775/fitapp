9388e9a6fc6dbfcf37986edec1c99681157ef404
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2022-10-18 09:01:10 +0200

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,10 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc, getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyA-ALBz3N6VaknteNY18ar1c3bq3Yenlh8",
     authDomain: "fitapp-5580c.firebaseapp.com",
@@ -13,7 +17,9 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
 
   export {
-firebase,
+firebase, db
 };
---
+++ src/views/Publish.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="hello">
+    <input type="file" @change="onFileSelected">
+    <button @click="onUpload">Upload</button>
+    </div>
+   </template>
+
+<script>
+export default{
+name:"Publish",
+data() {
+ return{
+        selectedFile: null
+    }
+},
+methods: {
+   onFileSelected(event) {
+   this.selectedFile=event.target.files[0]
+   },
+   onUpload() {
+
+   }
+}
+}
+import {firebase} from "@/firebase";
+</script>
