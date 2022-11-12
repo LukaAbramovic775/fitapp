86b05c309298892d0914650c5c3dd6d6bb57b475
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2022-11-12 15:34:21 +0100

    fix publish.vue

--- src/views/Publish.vue
+++ src/views/Publish.vue
@@ -1,33 +1,25 @@
-
 <template>
-<div class="row">
-    <div class="col-2"></div>
-    <div class="col-7">
-    <CBUM v-for="card in cards" :key="card.id" :info="card"/>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-7">
+        <CBUM v-for="card in cards" :key="card.id" :info="card"/>
+        </div>
+
+        <form @submit.prevent="postNewImage" class="mb-5">
+            <div class="form-group">
+                <croppa :width="400" :height="400" placeholder="Učitaj sliku.." v-model="imageReference"> </croppa>
+                <label for="imageDescription"> Description </label>
+                <input
+                    v-model="newImageDescription"
+                    type="text"
+                    class="form-control ml-2"
+                    placeholder="Enter the image description"
+                    id="imageDescription"
+                />
+            </div>
+        </form>
+        <button type="submit" class="btn btn-primary ml-2">Post image</button>
     </div>
-</div>
-
-    <form @submit.prevent="postNewImage" class="form-inline mb-5">
-
- <div class="form-group">
-
- <croppa :width="400" :height="400" placeholder="Učitaj sliku.." v-model="imageReference"> </croppa>
- 
-
- <div class="form-group">
- <label for="imageDescription"> Description </label>
- <input
- v-model="newImageDescription"
-type="text"
-class="form-control ml-2"
-placeholder="Enter the image description"
-id="imageDescription"
- />
- </div>
- </div>
- <button type="submit" class="btn btn-primary ml-2">Post
-image</button>
- </form>
 </template>
 
 <script>
@@ -35,87 +27,79 @@ import store from "@/store";
 import { db } from "@/firebase";
 import CBUM from "@/components/CBUM.vue";
 
-let cards=[];
+let cards = [];
 
-cards= [];
+//cards= [];
 
 
 export default {
-name: "publish",
-data: function() {
-    return {
-           cards: [],
-           store,
-           newImageDescription: "",
-           newImageUrl: "",
-           imageReference: null,
-    };
-},
+    name: "publish",
+    data: function() {
+        return {
+            cards: [],
+            store,
+            newImageDescription: "",
+            newImageUrl: "",
+            imageReference: null,
+        };
+    },
+    methods: {
 
-mounted() {
-    
-    this.getPosts();
-    //
-    //
-},
+        getPosts(){
 
-methods: {
+            console.log("firebase dohvat...");
 
-    getPosts() {
+            db.collection("posts")
+            .orderBy("posted at", "desc")
+            .limit(10)
+            .get()
+            .then((query) =>{ 
+                this.cards = [];
+                query.forEach((doc) =>{ 
+                    const data = doc.data();
+                    console.log(data);
 
-         console.log("firebase dohvat...");
+                    this.cards.push({
+                        id: doc.id,
+                        time: data.posted_at,
+                        description: data.desc,
+                        url: data.url,
 
-         db.collection("posts")
-         .orderBy("posted at", "desc")
-         .limit(10)
-         .get()
-         .then((query) =>{ 
-            this.cards= [];
-            query.forEach((doc) =>{ 
-                   const data = doc.data();
-                   console.log(data);
-
-                  this.cards.push({
-                       id: doc.id,
-                       time: data.posted_at,
-                       description: data.desc,
-                       url: data.url,
-
-                  });
+                    });
+                });
             });
-         });
+        },
+
+        postNewImage(){
+            const imageUrl = this.newImageUrl;
+            const imageDescription = this.newImageDescription;
+
+            db.collection("posts")
+            .add({
+                url: imageUrl,
+                desc: imageDescription,
+                email: store.currentUser,
+                posted_at: Date.now(),
+            })
+            .then(()=>{
+                console.log("spremljeno",doc);
+                this.newImageDescription="";
+                this.newImageUrl="";
+
+                this.getPosts();
+            })
+            .catch((e)=>{
+                console.error(e);
+            });
+        },
     },
+    computed: {
+        filteredCards() {
 
-    postNewImage(){
-    const imageUrl = this.newImageUrl;
-    const imageDescription = this.newImageDescription;
-
-db.collection("posts")
-.add({
-    url: imageUrl,
-    desc: imageDescription,
-    email: store.currentUser,
-    posted_at: Date.now(),
-   })
-   .then(()=>{
-    console.log("spremljeno",doc);
-    this.newImageDescription="";
-    this.newImageUrl="";
-
-    this.getPosts();
-   })
-   .catch((e)=>{
-    console.error(e);
-});
-    },
-  },
-  computed: {
-filteredCards() {
-
-    let termin= this.store.searchTerm;
-    
-    return this.cards.filter((card) => card.description.includes(temrin));
-  },
-},
+            let termin = this.store.searchTerm;
+            
+            return this.cards.filter((card) => card.description.includes(temrin));
+        }
+    }
 };
 </script>
