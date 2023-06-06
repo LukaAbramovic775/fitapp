311a24bd1406b03476267e94eb6ec455cb261f0b
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2023-06-06 20:04:47 +0200

    commit

--- src/views/Exercises.vue
+++ src/views/Exercises.vue
@@ -1,71 +1,72 @@
 <template>
-    <div class="container text-center">
-     
-         <div class="row">
-           <div class="col">
-             <BarbellRow />
-           </div>
-           <div class="col order-5">
-            <DumbbellFly />
-           </div>
-           <div class="col order-1">
-             <BarbellCurl />
-           </div>
-         </div>
+  <div class="container text-center">
    
-         <div class="row">
-           <div class="col">
-             <CableRow />
-           </div>
-           <div class="col order-5">
-            <BenchPress />
-           </div>
-           <div class="col order-1">
-             <HammerCurls />
-           </div>
-         </div>
-   
-         <div class="row">
-           <div class="col">
-             <Squat />
-           </div>
-           <div class="col order-5">
-            <RomanianDeadlift />
-           </div>
-           <div class="col order-1">
-             <Legpress />
-           </div>
-         </div>
-   
-   </div>   
-   </template>
-    
-    <script>
-    import BarbellRow from "@/components/BarbellRow.vue";
-    import BarbellCurl from "@/components/BarbellCurl.vue";
-    import DumbbellFly from "@/components/DumbbellFly.vue";
-    import CableRow  from "@/components/CableRow .vue";
-    import BenchPress from "@/components/BenchPress.vue";
-    import HammerCurls from "@/components/HammerCurls.vue";
-    import Squat from "@/components/Squat.vue";
-    import RomanianDeadlift from "@/components/RomanianDeadlift.vue";
-    import Legpress from "@/components/Legpress.vue";
-    
-    export default{
-           name: "Exercises",
-           components: {
-            BarbellCurl,
-            BarbellRow,
-            DumbbellFly,
-            CableRow,
-            BenchPress,
-            HammerCurls,
-            Squat,
-            RomanianDeadlift,
-            Legpress,
-           },
-    };
-    </script>
+    <div class="row">
+      <div class="col-md-4">
+        <BarbellRow/>
+      </div>
+      <div class="col-md-4">
+        <DumbbellFly/>
+      </div>
+      <div class="col-md-4">
+        <BarbellRow/>
+      </div>
+    </div>
+ 
+    <div class="row">
+      <div class="col-md-4">
+        <BarbellRow/>
+      </div>
+      <div class="col-md-4">
+        <DumbbellFly/>
+      </div>
+      <div class="col-md-4">
+        <BarbellRow/>
+      </div>
+    </div>
+
+ 
+    <div class="row">
+      <div class="col-md-4">
+        <BarbellRow/>
+      </div>
+      <div class="col-md-4">
+        <DumbbellFly/>
+      </div>
+      <div class="col-md-4">
+        <BarbellRow/>
+      </div>
+    </div>
+ 
+ </div>   
+ </template>
+  
+  <script>
+  import BarbellRow from "@/components/BarbellRow.vue";
+  import BarbellCurl from "@/components/BarbellCurl.vue";
+  import DumbbellFly from "@/components/DumbbellFly.vue";
+  import CableRow  from "@/components/CableRow.vue";
+  import BenchPress from "@/components/BenchPress.vue";
+  import HammerCurls from "@/components/HammerCurls.vue";
+  import Squat from "@/components/Squat.vue";
+  import RomanianDeadlift from "@/components/RomanianDeadlift.vue";
+  import Legpress from "@/components/Legpress.vue";
+  
+  export default{
+         name: "Exercises",
+         components: {
+          BarbellCurl,
+          BarbellRow,
+          DumbbellFly,
+          CableRow,
+          BenchPress,
+          HammerCurls,
+          Squat,
+          RomanianDeadlift,
+          Legpress,
+         },
+  };
+  </script>
 
 <style lang="scss">
 .card{
@@ -79,4 +80,3 @@ font-size:15px;
 }
 
 </style>
-    
